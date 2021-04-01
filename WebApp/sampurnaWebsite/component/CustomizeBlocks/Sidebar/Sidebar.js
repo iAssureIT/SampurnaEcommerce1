d30afa77cad3ba5b8632ae5fb384ed2441730c04
sanglesunita131 <sanglesunita131@gmail.com>
2021-04-01 20:41:47 +0530

    Sunita Worked on account page, photo gallery block, Return Policyblock

--- WebApp/sampurnaWebsite/component/CustomizeBlocks/Sidebar/Sidebar.js
+++ WebApp/sampurnaWebsite/component/CustomizeBlocks/Sidebar/Sidebar.js
@@ -24,59 +24,39 @@ export default class Sidebar extends Component {
     }
   	render() {  
     return (
-		<div className="sidebar col-lg-12 col-md-12 col-sm-12 col-xs-12 NoPadding">
-			{/* <div className="mp-title">
-		        <strong>Marketplace</strong>
-		    </div> */}
-		    {/* <div className="mp-subtitle">
-		        <strong>Marketplace</strong>
-		    </div>
-		    <nav className="account-nav">
-		            <ul className="nav items">
-		                <li className="nav item col-lg-12">
-		                <a href="http://demo8.cmsmart.net/mag2_amazon_themeforest/france/customer/account/">
-		                <i className="fa fa-user" aria-hidden="true"></i>&nbsp; Become a Seller</a>
-		                </li>
-		            </ul>
-		    </nav>     */}
+		<div className="sidebar col-lg-12 col-md-12 col-sm-12 col-xs-12 NoPadding">			
 		    <br className="hidden-xs" />
 		    <br className="hidden-xs" />        
       			<nav className="account-nav">
 		            <ul className="nav items">
-		                <li className="nav item col-lg-12 col-md-12 col-sm-12 col-xs-12 NOpadding">
+		                <li className="nav item col-12 ">
 		                <a href="/account" id="atag"><i className="fa fa-user" aria-hidden="true"></i>&nbsp; Account Dashboard</a>
 		                </li>
 
-		                <li className="nav item col-lg-12 col-md-12 col-sm-12 col-xs-12 NOpadding">
+		                <li className="nav item col-12">
 		                <a href="/edit-account" id="atag"><i className="fa fa-info" aria-hidden="true"></i>&nbsp; Account Information</a>
 		                </li>
 
-		                <li className="nav item col-lg-12 col-md-12 col-sm-12 col-xs-12 NOpadding">
+		                <li className="nav item col-12">
 		                <a href="/address-book" id="atag"><i className="fa fa-location-arrow"></i>&nbsp; Address Book</a>
 		                </li>
 
-		                <li className="nav item col-lg-12 col-md-12 col-sm-12 col-xs-12 NOpadding">
+		                <li className="nav item col-12">
 		                <a href="/my-orders" id="atag"><i className="fa fa-download"></i>&nbsp; My Orders </a>
 		                </li>
 
-		                <li className="nav item col-lg-12 col-md-12 col-sm-12 col-xs-12 NOpadding">
+		                <li className="nav item col-12">
 		                <a href="/wishlist" id="atag"><i className="fa fa-heart"></i> &nbsp; My Wishlist</a>
 		                </li>		                
 		               
-		                <li className="nav item col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		                <li className="nav item col-12">
 		                <a href="/productreview" id="atag"><i className="fa fa-eye"></i> &nbsp;My Product Reviews</a>
 		                </li>
 		                          
 		            </ul>
 		        </nav>
 		        <br className="hidden-xs" />
-		       	{/*<div className="block block-compare" >
-		       					    <div className="block-title">
-		       					        <strong id="block-compare-heading" role="heading" aria-level="2">Compare Products</strong>
-		       					    </div>
-		       					    <hr/>
-		       					    <div className="empty">You have no items to compare.</div>
-		       					</div>*/}
+		       	
       		</div>
       		);
 	}
--- WebApp/sampurnaWebsite/pages/account/index.js
+++ WebApp/sampurnaWebsite/pages/account/index.js
@@ -96,108 +96,111 @@ class Account extends Component{
                 <Address addressId={this.state.addressId} opDone={this.opDone.bind(this)}/>
                 <div className="container">
                     <br/> 
-                    <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                        <Sidebar />
-                    </div>
-                    <div className="col-lg-9 col-md-9 col-sm-9 col-xs-12 NOpadding mt25">
-                        <h4 className="accountTitle">Account Dashboard</h4>
-                        <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt15 mb15 mobileViewNoPadding">
-                            <p><label className="col-lg-12 col-md-12 col-sm-12 col-xs-12">Hello {this.state.firstName}</label></p>                            
+                    <div className="row">
+                        <div className="col-12 col-lg-3 ">
+                            <Sidebar />
+                        </div>
+                        <div className="col-12 col-lg-9 mt25">
+                            <h4 className="accountTitle">Account Dashboard</h4>
+                            <p><label className="col-12">Hello {this.state.firstName}</label></p>
+                            <div className="col-12 row mt15 mb15 mobileViewNoPadding">
+                                                            
 
-                            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 mt15 mb15 mobileViewNoPadding">
-                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 accountBox">
-                                    <div className="row">
-                                        <div className="accountDivHeader">Contact Information</div>
-                                        <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt25 mb25">
-                                            <p className="col-lg-12 col-md-12 col-sm-12 col-xs-12 textwrap">{this.state.fullName}</p>
-                                            <p className="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">{this.state.emailId}</p>
-                                            <p className="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">{this.state.mobileNumber}</p>
-                                            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                                <button className="btn globalCommonBtn col-lg-6 col-md-6 col-sm-6 col-xs-12" onClick={this.editUser.bind(this)}><i className="fa fa-pencil-square-o"></i> &nbsp; EDIT</button> &nbsp; &nbsp;
-                                            {/*<button className="btn btn-warning">CHANGE PASSWORD</button>*/}
+                                <div className="col-12 col-lg-6 mt15 mb15 mobileViewNoPadding">
+                                    <div className="col-12 accountBox">
+                                        <div className="row">
+                                            <div className="accountDivHeader">Contact Information</div>
+                                            <div className="col-12 mt25 mb25">
+                                                <p className="col-12 textwrap">{this.state.fullName}</p>
+                                                <p className="col-12 ">{this.state.emailId}</p>
+                                                <p className="col-12 ">{this.state.mobileNumber}</p>
+                                                <div className="col-12 mt100">
+                                                    <button className="btn globalCommonBtn col-12 col-sm-6 " onClick={this.editUser.bind(this)}><i className="fa fa-pencil-square-o"></i> &nbsp; EDIT</button> &nbsp; &nbsp;
+                                                {/*<button className="btn btn-warning">CHANGE PASSWORD</button>*/}
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 mt15 mb15 mobileViewNoPadding">
-                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 accountBox">
-                                    <div className="row">
-                                        <div className="accountDivHeader">Newsletters</div>
-                                        <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt25 mb25">
-                                            <p className="col-lg-12 col-md-12 col-sm-12 col-xs-12">You don't subscribe to our newsletter.</p>
-                                            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt45">
-                                                <button className="btn globalCommonBtn col-lg-6 col-md-6 col-sm-6 col-xs-12"><i className="fa fa-pencil-square-o"></i> &nbsp; EDIT</button>
+                                <div className="col-12 col-lg-6 mt15 mb15 mobileViewNoPadding">
+                                    <div className="col-12 accountBox">
+                                        <div className="row">
+                                            <div className="accountDivHeader">Newsletters</div>
+                                            <div className="col-12 mt25 mb25">
+                                                <p className="col-12">You don't subscribe to our newsletter.</p>
+                                                <div className="col-12 mt45">
+                                                    <button className="btn globalCommonBtn col-12 col-sm-6 "><i className="fa fa-pencil-square-o"></i> &nbsp; EDIT</button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <label className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt15 mb15 row">Address Book</label> 
-                            </div>
-                            
-                            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 mt15 mobileViewNoPadding">
-                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 accountBox">
-                                    <div className="row">
-                                        <div className="accountDivHeader">Default Billing Address</div>
-                                        { this.state.addressLine1 ?
-                                            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt25 mb25">
-                                                <p className="col-lg-12 col-md-12 col-sm-12 col-xs-12">{this.state.name}</p>
-                                                <p className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                                {this.state.addressLine2} - &nbsp;
-                                                {this.state.addressLine1} - &nbsp;
-                                                {this.state.pincode},<br />                                                
-                                                {/* {this.state.city},<br />
-                                                {this.state.state}, {this.state.country} - {this.state.pincode}<br /> */}
-                                                Contact Number: {this.state.mobileNumber}
-                                                </p>
-                                                
-                                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                                    <div data-toggle="modal" data-target="#checkoutAddressModal" onClick={this.getAddressId.bind(this)} id={this.state.deliveryAddressID} className="btn globalCommonBtn"><i className="fa fa-pencil-square-o"></i> &nbsp; EDIT ADDRESS</div>
+                                <div className="col-12">
+                                    <label className="col-12 mt15 mb15 row">Address Book</label> 
+                                </div>
+                                
+                                <div className="col-12 col-lg-6 mt15 mobileViewNoPadding">
+                                    <div className="col-12 accountBox">
+                                        <div className="row">
+                                            <div className="accountDivHeader">Default Billing Address</div>
+                                            { this.state.addressLine1 ?
+                                                <div className="col-12 mt25 mb25">
+                                                    <p className="col-12">{this.state.name}</p>
+                                                    <p className="col-12">
+                                                    {this.state.addressLine2} - &nbsp;
+                                                    {this.state.addressLine1} - &nbsp;
+                                                    {this.state.pincode},<br />                                                
+                                                    {/* {this.state.city},<br />
+                                                    {this.state.state}, {this.state.country} - {this.state.pincode}<br /> */}
+                                                    Contact Number: {this.state.mobileNumber}
+                                                    </p>
+                                                    
+                                                    <div className="col-12">
+                                                        <div data-toggle="modal" data-target="#checkoutAddressModal" onClick={this.getAddressId.bind(this)} id={this.state.deliveryAddressID} className="btn globalCommonBtn"><i className="fa fa-pencil-square-o"></i> &nbsp; EDIT ADDRESS</div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            :
-                                            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt25 mb25">
-                                                <p className="col-lg-12 col-md-12 col-sm-12 col-xs-12">You have not set a default billing address.</p>
-                                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt25">
-                                                    <button data-toggle="modal" data-target="#checkoutAddressModal"
-                                                     className="btn globalCommonBtn col-lg-6 col-md-6 col-sm-6 col-xs-12"><i className="fa fa-pencil-square-o"></i> &nbsp; ADD ADDRESS</button>
+                                                :
+                                                <div className="col-12 mt25 mb25">
+                                                    <p className="col-12">You have not set a default billing address.</p>
+                                                    <div className="col-12 mt25">
+                                                        <button data-toggle="modal" data-target="#checkoutAddressModal"
+                                                         className="btn globalCommonBtn col-12 col-sm-6"><i className="fa fa-pencil-square-o"></i> &nbsp; ADD ADDRESS</button>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        }
+                                            }
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            
-                            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 mt15 mb15 mobileViewNoPadding">
-                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 accountBox">
-                                    <div className="row">
-                                        <div className="accountDivHeader">Default Shipping Address</div>
-                                        { this.state.addressLine1 ?
-                                            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt25 mb25">
+                                
+                                <div className="col-12 col-lg-6 mt15 mb15 mobileViewNoPadding">
+                                    <div className="col-12 accountBox">
+                                        <div className="row">
+                                            <div className="accountDivHeader">Default Shipping Address</div>
+                                            { this.state.addressLine1 ?
+                                                <div className="col-12 mt25 mb25">
 
-                                                <p className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                                {this.state.addressLine2 ? this.state.addressLine2+"-" : null} &nbsp;
-                                                {this.state.addressLine1}- &nbsp;
-                                                {this.state.pincode}<br />                                                
-                                                Email: {this.state.emailId}<br />
-                                                Contact Number: {this.state.mobileNumber} 
-                                                </p>
-                                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                                    <div data-toggle="modal" data-target="#checkoutAddressModal" onClick={this.getAddressId.bind(this)} id={this.state.deliveryAddressID} className="btn globalCommonBtn"><i className="fa fa-pencil-square-o"></i> &nbsp; EDIT ADDRESS</div>
+                                                    <p className="col-12">
+                                                    {this.state.addressLine2 ? this.state.addressLine2+"-" : null} &nbsp;
+                                                    {this.state.addressLine1}- &nbsp;
+                                                    {this.state.pincode}<br />                                                
+                                                    Email: {this.state.emailId}<br />
+                                                    Contact Number: {this.state.mobileNumber} 
+                                                    </p>
+                                                    <div className="col-12">
+                                                        <div data-toggle="modal" data-target="#checkoutAddressModal" onClick={this.getAddressId.bind(this)} id={this.state.deliveryAddressID} className="btn globalCommonBtn"><i className="fa fa-pencil-square-o"></i> &nbsp; EDIT ADDRESS</div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            :
-                                            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt25 mb15">
-                                                <p className="col-lg-12 col-md-12 col-sm-12 col-xs-12" style={{wordBreak : "break-word"}}>You have not set a default shipping/billing address.</p>
-                                                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 mt15">
-                                                    <button data-toggle="modal" data-target="#checkoutAddressModal" className="btn globalCommonBtn col-lg-6 col-md-6 col-sm-6 col-xs-12"><i className="fa fa-pencil-square-o"></i> &nbsp; ADD ADDRESS</button>
+                                                :
+                                                <div className="col-12 mt25 mb15">
+                                                    <p className="col-12" style={{wordBreak : "break-word"}}>You have not set a default shipping/billing address.</p>
+                                                    <div className="col-12 mt15">
+                                                        <button data-toggle="modal" data-target="#checkoutAddressModal" className="btn globalCommonBtn col-12 col-sm-6"><i className="fa fa-pencil-square-o"></i> &nbsp; ADD ADDRESS</button>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        }
+                                            }
+                                        </div>
                                     </div>
                                 </div>
                             </div>
