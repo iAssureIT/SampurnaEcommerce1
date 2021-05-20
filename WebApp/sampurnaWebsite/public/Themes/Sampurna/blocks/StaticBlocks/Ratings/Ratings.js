1eb3f73a1c1116ec8c37fada66bd08d8898ffc1e
priya-chavan <priya.yogesh.toke@gmail.com>
2021-05-20 19:38:03 +0400

    Priya worked on delevery location and vebdor list page - 2

--- WebApp/sampurnaWebsite/public/Themes/Sampurna/blocks/StaticBlocks/Ratings/Ratings.js
+++
@@ -1,5 +0,0 @@
-// import { Component } from "react";
-// import Style from './Rating.module.css';
-// import { FaStar } from 'react-icons/fa';
-// "react-star-rating-component": "^1.4.1",
-// "react-icons": "^4.2.0",
