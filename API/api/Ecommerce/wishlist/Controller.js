de43f7bff7cebf0ec0218f7cda483f2aed587686
KhedkarJyoti <khedkarjyoti1995@gmail.com>
2021-08-24 12:46:01 +0530

    Jyoti added changes in orders

--- API/api/Ecommerce/wishlist/Controller.js
+++ API/api/Ecommerce/wishlist/Controller.js
@@ -160,54 +160,52 @@ exports.get_user_wishlist = (req,res,next)=>{
             maxDistanceRadius = maxKmRadius.maxRadius;
         }
         for (var i = 0; i < wishdata.length; i++) {
-            console.log("wishdata[i] => ",wishdata[i])
-            console.log("wishdata[i].product_ID.productCode => ",wishdata[i].product_ID.productCode)
-            console.log("wishdata[i].product_ID.itemCode => ",wishdata[i].product_ID.itemCode)
-            console.log("wishdata[i].product_ID.vendor_ID => ",wishdata[i].product_ID.vendor_ID)
-            var inventoryData = await ProductInventory.findOne({productCode : wishdata[i].product_ID.productCode, itemCode : wishdata[i].product_ID.itemCode, vendor_ID : ObjectId(wishdata[i].product_ID.vendor_ID)},{currentQuantity : 1})
-			console.log("inventoryData => ",inventoryData)
+            if (wishdata[i].product_ID !== null) {
+                var inventoryData = await ProductInventory.findOne({productCode : wishdata[i].product_ID.productCode, itemCode : wishdata[i].product_ID.itemCode, vendor_ID : ObjectId(wishdata[i].product_ID.vendor_ID)},{currentQuantity : 1})
+    			console.log("inventoryData => ",inventoryData)
 
-            var product = {
-                vendor_id           : wishdata[i].vendor_id,
-                vendorLocation_id   : wishdata[i].vendorLocation_id,
-                _id                 : wishdata[i].product_ID._id,
-                productName         : wishdata[i].product_ID.productName,
-                productNameRlang    : wishdata[i].product_ID.productNameRlang,
-                brandNameRlang      : wishdata[i].product_ID.brandNameRlang,
-                productUrl          : wishdata[i].product_ID.productUrl,
-                availableQuantity	: inventoryData && inventoryData !== null ? inventoryData.currentQuantity : 0,
-                originalPrice       : wishdata[i].product_ID.originalPrice,
-                size                : wishdata[i].product_ID.size,
-                shortDescription    : wishdata[i].product_ID.shortDescription,
-                unit                : wishdata[i].product_ID.unit, 
-                brand               : wishdata[i].product_ID.brand,
-                section             : wishdata[i].product_ID.section,
-                category            : wishdata[i].product_ID.category,
-                subCategory         : wishdata[i].product_ID.subCategory,
-                section_ID          : wishdata[i].product_ID.section_ID,
-                category_ID         : wishdata[i].product_ID.category_ID,
-                subCategory_ID      : wishdata[i].product_ID.subCategory_ID,
-                currency            : wishdata[i].product_ID.currency,
-                discountPercent     : wishdata[i].product_ID.discountPercent,
-                discountedPrice     : wishdata[i].product_ID.discountedPrice,
-                productCode         : wishdata[i].product_ID.productCode,
-                productImage        : wishdata[i].product_ID.productImage,
-                wishlist_ID         : wishdata[i]._id,
-                isWish              : true
-            }
-
-            var areaIndex = returnData.findIndex(record => record.areaName === wishdata[i].userDelLocation.delLocation);
-            if(areaIndex >= 0){
-                returnData[areaIndex].products.push(product)
-            }else{
-                var returnDataObject = {
-                    areaName            : wishdata[i].userDelLocation.delLocation,
-                    distance            : await calcUserDist(req.body.userLat, req.body.userLong, wishdata[i].userDelLocation.lat, wishdata[i].userDelLocation.long),
-                    maxDistanceRadius   : maxDistanceRadius,
-                    products            : [product]
+                var product = {
+                    vendor_id           : wishdata[i].vendor_id,
+                    vendorLocation_id   : wishdata[i].vendorLocation_id,
+                    _id                 : wishdata[i].product_ID._id,
+                    productName         : wishdata[i].product_ID.productName,
+                    productNameRlang    : wishdata[i].product_ID.productNameRlang,
+                    brandNameRlang      : wishdata[i].product_ID.brandNameRlang,
+                    productUrl          : wishdata[i].product_ID.productUrl,
+                    availableQuantity	: inventoryData && inventoryData !== null ? inventoryData.currentQuantity : 0,
+                    originalPrice       : wishdata[i].product_ID.originalPrice,
+                    size                : wishdata[i].product_ID.size,
+                    shortDescription    : wishdata[i].product_ID.shortDescription,
+                    unit                : wishdata[i].product_ID.unit, 
+                    brand               : wishdata[i].product_ID.brand,
+                    section             : wishdata[i].product_ID.section,
+                    category            : wishdata[i].product_ID.category,
+                    subCategory         : wishdata[i].product_ID.subCategory,
+                    section_ID          : wishdata[i].product_ID.section_ID,
+                    category_ID         : wishdata[i].product_ID.category_ID,
+                    subCategory_ID      : wishdata[i].product_ID.subCategory_ID,
+                    currency            : wishdata[i].product_ID.currency,
+                    discountPercent     : wishdata[i].product_ID.discountPercent,
+                    discountedPrice     : wishdata[i].product_ID.discountedPrice,
+                    productCode         : wishdata[i].product_ID.productCode,
+                    productImage        : wishdata[i].product_ID.productImage,
+                    wishlist_ID         : wishdata[i]._id,
+                    isWish              : true
                 }
-                returnData.push(returnDataObject);   
-            }                 
+
+                var areaIndex = returnData.findIndex(record => record.areaName === wishdata[i].userDelLocation.delLocation);
+                if(areaIndex >= 0){
+                    returnData[areaIndex].products.push(product)
+                }else{
+                    var returnDataObject = {
+                        areaName            : wishdata[i].userDelLocation.delLocation,
+                        distance            : await calcUserDist(req.body.userLat, req.body.userLong, wishdata[i].userDelLocation.lat, wishdata[i].userDelLocation.long),
+                        maxDistanceRadius   : maxDistanceRadius,
+                        products            : [product]
+                    }
+                    returnData.push(returnDataObject);   
+                }
+            }               
         }
         if(i >= wishdata.length){
             res.status(200).json(returnData);  
