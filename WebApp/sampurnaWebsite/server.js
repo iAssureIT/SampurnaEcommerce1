2a65ccd4cfe586faacee9c1bc68a021aee6fa7b2
iAssureIT <iassureit@gmail.com>
2021-09-01 19:47:57 +0530

    AN changed server.js

--- WebApp/sampurnaWebsite/server.js
+++ WebApp/sampurnaWebsite/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const next = require('next')
+const conf = require('./next.config')
 
-const port = parseInt(process.env.PORT, 10) || 3006
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+// const port = parseInt(process.env.PORT, 10) || 3006
+const port = conf.publicRuntimeConfig.PORT;
+// const dev = process.env.NODE_ENV !== 'production'
+const dev = conf.publicRuntimeConfig.NODE_ENV !== 'production';
+console.log("dev = ",dev);
+
+const app = next({ dev });
 
 const handle = app.getRequestHandler()
 
